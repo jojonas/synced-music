b31b4a3dea81ed8ca4a9c375eb1a5cc781e82628
jojonas <lieb.jonas@gmail.com>
2013-08-01 23:00:42 +0200

    added HighPrecisionTimer

---
+++ synced-music/core/util/timer.py
@@ -0,0 +1,74 @@
+import time, datetime
+import platform
+
+def linear_regression(x, y):
+	Sxx = sum(xi*xi for xi in x)
+	Sy = sum(y)
+	Sx = sum(x)
+	Sxy = sum(xi*yi for xi, yi in zip(x,y))
+	
+	S = len(x)
+	
+	delta = S*Sxx - Sx*Sx
+	
+	a = (Sxx*Sy - Sx*Sxy)/delta
+	m = (S*Sxy - Sx*Sy)/delta
+	
+	return (a,m)
+
+if platform.system() == "Windows":
+	class HighPrecisionTimer:
+		def __init__(self):
+			self.data_time = []
+			self.data_clock = []
+
+			self.minimum_data_count = 5
+			self.ring_size = 20
+
+			self.a = time.time()
+			self.m = 1
+			
+			self.update()
+			
+		def update(self):
+			self.data_clock.append(time.clock())
+			self.data_time.append(time.time())
+
+			self.data_clock = self.data_clock[-self.ring_size:]
+			self.data_time = self.data_time[-self.ring_size:]
+			self._updateRegression()
+		
+		def _updateRegression(self):
+			if len(self.data_clock) > 1:
+				self.a, self.m = linear_regression(self.data_clock, self.data_time)
+			
+		def time(self):
+			if len(self.data_clock) > self.minimum_data_count:
+				return time.clock() * self.m + self.a
+			else:
+				return time.clock() + self.a
+			
+		def status(self):
+			return "Training data length: %d" % len(self.data_time)
+			
+else:
+	class HighPrecisionTimer:
+		def update(self):
+			pass
+			
+		def time(self):
+			return time.time()
+		
+		def status(self):
+			return "No training data, running with time.time()"
+		
+
+if __name__=="__main__":
+	import random
+	timer = HighPrecisionTimer()
+	for i in xrange(100):
+		if i % 10 == 0:
+			timer.update()
+		print "%.5f" % timer.time()
+		time.sleep(random.random()*0.01)
+			
