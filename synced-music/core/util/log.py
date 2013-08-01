e651dc993ea474932488c1b24f4809b6375014e4
jojonas <lieb.jonas@gmail.com>
2013-08-01 23:14:49 +0200

    added pyQt logging

--- synced-music/core/util/log.py
+++ synced-music/core/util/log.py
@@ -1,23 +1,20 @@
-import Tkinter
+from PyQt4 import QtGui
 import logging
 
-class TextLog(Tkinter.Text, logging.Handler):
-	def __init__(self, master=None):
-		Tkinter.Text.__init__(self, master=master)
+class TextLog(QtGui.QListWidget, logging.Handler):
+	def __init__(self, parent=None):
+		QtGui.QListWidget.__init__(self, parent)
 		logging.Handler.__init__(self)
 
 		logger = logging.getLogger(__name__)
 		logger.addHandler(self)
 
-		frm = logging.Formatter("%(asctime)s %(levelname)s: %(message)s", "%d.%m.%Y %H:%M:%S") 
+		frm = logging.Formatter("%(asctime)s.%(msecs)d %(filename)s:%(lineno)d %(levelname)s :: %(message)s", "%d.%m.%Y %H:%M:%S") 
 		self.setFormatter(frm)
 
 	def handle(self, record):
 		msg = self.format(record)
-		self.insert(Tkinter.END, msg + "\n")
-
-	def flush(self):
-		self.update_idletasks()
+		self.addItem(msg)
 
 def getLogger():
 	return logging.getLogger(__name__)
