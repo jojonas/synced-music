20ce919140a4a06ff2cef9ea35eba58d7c09ccf1
Joel S <joelschum@gmail.com>
2013-08-01 23:45:13 +0200

    Added basic tcp server

---
+++ synced-music/core/client/network.py
@@ -0,0 +1,4 @@
+from ..util import network as sharednet
+
+class SyncedMusicClient(object):
+	def __init__(self):
---
+++ synced-music/core/server/network.py
@@ -0,0 +1,63 @@
+import socket
+import select
+import threading
+import random
+
+from ..util import network as sharednet
+from ..util import timer
+
+class SyncedMusicServer(threading.Thread):
+	def __init__(self, logger):
+		threading.Thread.__init__(self)
+		self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.serverSocket.bind(('', sharednet.PORT))
+		self.serverSocket.listen(4)
+		
+		self.quitFlag = threading.Event()
+		self.readSocketList = [self.serverSocket]
+		self.logger = logger
+		self.hptimer = timer.HighPrecisionTimer()
+
+		self.nextTimerUpdate = 0
+		self.nextSendTimestamp = 0
+		self.sendTimestampInterval = 0.8
+		self.nextSendChunk = 0
+		self.sendChunkInterval = 1.0
+
+	def quit(self):
+		self.quitFlag.set()
+
+	def run(self):
+		while not self.quitFlag.isSet():
+			readable, writeable, error = select.select(self.readSocketList, [], [], 0)
+			for sock in readable:
+				if sock is self.serverSocket:
+					clientSocket, address = sock.accept()
+					self.readSocketList.append(clientSocket)
+					self.logger.info("Client connected from " + str(address))
+				else:
+					data = sock.recv(1024)
+					if data:
+						self.logger.warning("Received data (this shouldn't happen...): " + data)
+
+			currentTime = self.hptimer.time()
+			
+			# update timer from TIME TO TIME! HA!
+			if self.nextTimerUpdate <= currentTime:
+				self.hptimer.update()
+				self.nextTimerUpdate = currentTime + random.random()
+
+			# Send timestamp
+			if self.nextSendTimestamp <= currentTime:
+				packet = struct.pack("Bf", sharednet.TIMESTAMP_PACKET_ID, currentTime)
+
+				for sock in self.readSocketList:
+					if sock is not self.serverSocket:
+						sock.send(packet)
+					
+				self.nextSendTimestamp = currentTime + self.sendTimestampInterval
+
+			# Send chunk
+			if self.nextSendChunk <= currentTime:
+				self.nextSendChunk = self.sendChunkInterval
